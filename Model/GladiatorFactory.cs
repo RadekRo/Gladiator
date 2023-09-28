0437d52dcde109c46e87fd06c5a8a833b4c84024
RadekRo <r.roclawski@ankara.pl>
2023-09-28 16:32:45 +0200

    Deleted Data dir changed names.txt to enum RomanNames

--- Model/GladiatorFactory.cs
+++ Model/GladiatorFactory.cs
@@ -17,25 +17,6 @@ namespace Gladiator.Model
         {
             int randomNumber = RandomGenerator.GetRandomNumber(1, 5);
 
-            string currentDirectory = Directory.GetCurrentDirectory();
-            
-            Console.WriteLine("Current Directory:");
-            Console.WriteLine(currentDirectory);
-
-            // Go up three levels
-            string threeLevelsUp = currentDirectory;
-
-            for (int i = 0; i < 3; i++)
-            {
-                threeLevelsUp = Directory.GetParent(threeLevelsUp).FullName;
-            }
-
-            Console.WriteLine("\nThree Levels Up:");
-            var myString = threeLevelsUp.ToString() + "\\Data\\Names.txt";
-
-            var namesList = DataReader.getDataFromFile(myString);
-            Console.WriteLine(namesList[0]);
-
             Type selectedType = GladiatorFactory.GetGladiatorType(randomNumber);
             BaseGladiator newGladiator = (BaseGladiator)Activator.CreateInstance(selectedType, selectedType.Name, "Radek");
             return newGladiator;
